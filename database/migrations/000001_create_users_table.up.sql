2c65fd9a4ff105d6d8a1545fcd9666596b366b92
habil-GIC <habil@gic-indonesia.com>
2024-03-13 12:11:08 +0700

    fix(migration): change email to name

--- database/migrations/000001_create_users_table.up.sql
+++ database/migrations/000001_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users(
    user_id serial PRIMARY KEY,
-   username VARCHAR (50) UNIQUE NOT NULL,
-   password VARCHAR (150) NOT NULL,
-   email VARCHAR (300) UNIQUE NOT NULL
+   username VARCHAR (20) UNIQUE NOT NULL,
+   name VARCHAR (100) NOT NULL,
+   password VARCHAR (150) NOT NULL
 );
